5698c3ed794ae9d6d3c8133a68db239a10be898b
Artem Borodovoy <a.borodovoi@gmail.com>
2024-07-24 23:27:05 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <h1>
+      {/* Home */}
+      <Button>Корзина</Button>
+      <Button variant="outline">Корзина</Button>
+    </h1>
+  );
+}
